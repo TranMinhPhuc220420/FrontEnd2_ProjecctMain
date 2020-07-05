dcd464d7703550885c287f1bf7994d46025a34c8
Trần Minh Phúc <50334644+TranMinhPhuc220420@users.noreply.github.com>
2020-07-06 00:36:48 +0700

    Phuc upload project empty normal

---
+++ src/components/Greet.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Greet = () => {
+  return <h1>Hello Best Team Front End 2</h1>
+}
+
+export default Greet;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greet from './components/Greet';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Greet />
+ </React.StrictMode>,
+  document.getElementById('root')
+);

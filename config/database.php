4b0614373b680f1b6c9647527c86e16064ff9740
Trần Minh Phúc <50334644+TranMinhPhuc220420@users.noreply.github.com>
2020-07-12 12:25:55 +0700

    Create database(sql) vs config model

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'skynew');
+
+//define('DB_HOST', 'localhost');
+//define('DB_USER', 'id13223573_thefacefake210920');
+//define('DB_PASSWORD', 'Ztegc1c2fa-99');
+//define('DB_NAME', 'id13223573_thefacefake');
+
+// define('DB_HOST', 'sql112.epizy.com');
+// define('DB_USER', 'epiz_25499455');
+// define('DB_PASSWORD', 'zQdxFxqfp6W1lif');
+// define('DB_NAME', 'epiz_25499455_skynews');
